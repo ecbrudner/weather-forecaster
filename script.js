6d727e8070a6c7c5ef74cf51c2324e921953e3c6
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2023-12-11 10:58:29 -0600

    created for loop for 5 day weather forecast in javascript

--- script.js
+++ script.js
@@ -19,9 +19,12 @@ function getWeather(){
         console.log(data);
         var lon= data[0].lon;
         var lat= data[0].lat;
+        var cityName= data[0].name;
         console.log(lon);
         console.log(lat);
-        
+        console.log(cityName);
+        // searchResultEl.innerHTML= cityName;
+
         //use lon and lat to get weather data
         var getWeatherDataUrl= "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid=b95e856ef0726d8cf81fd93906d37be3";
         console.log(getWeatherDataUrl);
@@ -32,6 +35,38 @@ function getWeather(){
             })
             .then (function(data){
                 console.log(data);
+                // assign all weather data to variables for today
+                var todayDate= data.list[0].dt_txt;
+                var todayIconUrl= "http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
+                var todayIcon= document.createElement("img");
+                var todayTemp= data.list[0].main.temp;
+                var todayWind= data.list[0].wind.speed;
+                var todayHumidity= data.list[0].main.humidity+ "%";
+                var weatherData= document.createElement("div");
+
+                //append today's temp, wind, humdity to searchResultEl
+                searchResultEl.innerHTML= cityName + " " + todayDate;
+                todayIcon.setAttribute("src", todayIconUrl);
+                searchResultEl.appendChild(todayIcon);
+                weatherData.innerHTML= "Temp: "+ todayTemp + " Wind: " + todayWind + " Humidity: " + todayHumidity;
+                searchResultEl.appendChild(weatherData);
+
+                //append 5 day forecast to weekForecastEl
+                for (var i=8; i<40; i+=8){
+                    var dayForecast= document.createElement("div");
+                    var dayDate= data.list[i].dt_txt;
+                    var dayIconUrl= "http://openweathermap.org/img/w/"+data.list[i].weather[0].icon+".png";
+                    var dayIcon= document.createElement("img");
+                    var dayTemp= data.list[i].main.temp;
+                    var dayWind= data.list[i].wind.speed;
+                    var dayHumidity= data.list[i].main.humidity+ "%";
+                    dayForecast.innerHTML= dayDate;
+                    dayIcon.setAttribute("src", dayIconUrl);
+                    dayForecast.appendChild(dayIcon);
+                    dayForecast.innerHTML+= "Temp: "+ dayTemp + " Wind: " + dayWind + " Humidity: " + dayHumidity;
+                    weekForecastEl.appendChild(dayForecast);
+                }
+        
             })
              .catch(function(error){
                 console.log(error);
@@ -40,7 +75,12 @@ function getWeather(){
     .catch(function(error){
         console.log(error);
     });
-
-   //append today's temp, wind, humdity to searchResultEl
-   //append 5 day forecast to weekForecastEl
 }
+
+
+//0=right now
+//8= 24 hours from now (day1)
+//16= 48 hours from now (day2)
+//24= 72 hours from now (day3)
+//32= 96 hours from now (day4)
+//39= 120 hours from now (day5)
